0bb0b6f76bafe52980eea57e808443e8e25117ed
KCynide <jyl5099@gmail.com>
2023-08-10 18:12:05 +0900

    시작합니다
    전기폭풍군단호

---
+++ electricarmy/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>전기폭풍군단 가보즈아</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
